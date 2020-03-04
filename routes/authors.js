7144544fc84971ef5a312f93eb817cb6395f6b06
Ryszard Matula <ryszard.fragmin@gmail.com>
2020-03-04 13:51:26 +0100

    Viewing authors

--- routes/authors.js
+++ routes/authors.js
@@ -4,8 +4,21 @@ const Author = require('../models/author')
 
 
 // All Authors Route
-router.get('/', (req, res) => {
-    res.render('authors/index')
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name !== '') {
+        searchOptions.name = new RegExp(req.query.name, 'i') // i - case insensitive
+    }
+    try {
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', {
+            authors: authors,
+            searchOptions: req.query
+        })
+    }
+    catch{
+        res.redirect('/')
+    }
 })
 
 // New Author Route
@@ -14,8 +27,20 @@ router.get('/new', (req, res) => {
 })
 
 // Create Author Route
-router.post('/', (req, res) => {
-    res.send(req.body.name)
+router.post('/', async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+    try {
+        const newAuthor = await author.save()
+        //res.redirect(`authors/${newAuthor.id}`)
+        res.redirect(`authors`)
+    } catch{
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error creating Author'
+        })
+    }
 })
 
 module.exports = router;
